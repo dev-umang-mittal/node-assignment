84c0f6f8237eb4b7c82bf0b5acc3a9934847ee37
com194 <umang.mittal@daffodilsw.com>
2022-02-25 16:05:50 +0530

    Added mongodb queries for fetching data

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ app.post("/create", async (req, res) => {
 });
 
 // Returns the user specific to passed ID.
-app.get("/user/:id", (req, res) => {
+app.get("/user/:id", async (req, res) => {
   let resp = {},
     code;
   try {
@@ -81,39 +81,59 @@ app.get("/user/:id", (req, res) => {
 });
 
 // Edits the specific user with the passed ID.
-app.put("/user/:id", getUserIndex, (req, res) => {
-  users[req.body.arrIndex] = { ...req.body.user, ...req.body.edit };
-  res.send({ code: 202, status: "User Edited Successfully" });
+app.put("/user/:id", async (req, res) => {
+  let code;
+  try {
+    await connection
+      .db("users")
+      .collection("users")
+      .updateOne({ _id: req.params.id }, { $set: req.body });
+    code = 200;
+  } catch (e) {
+    code = 400;
+    console.log(e);
+  } finally {
+    res.sendStatus(code);
+  }
 });
 
 // Deletes a user specific to the passed ID.
-app.delete("/user/:id", getUserIndex, (req, res) => {
-  users.splice(req.body.arrIndex, 1);
-  res.send({ code: 202, status: "User Deleted Successfully" });
+app.delete("/user/:id", async (req, res) => {
+  let code;
+  try {
+    await connection
+      .db("users")
+      .collection("users")
+      .deleteOne({ _id: req.params.id });
+    code = 202;
+  } catch (e) {
+    code = 400;
+    console.log(e);
+  } finally {
+    res.sendStatus(code);
+  }
 });
 
 //Autenticating a user based on email and password
-app.post("/login", authenticate, (req, res) => {
-  res.send({ code: 200, status: "User LoggedIn Successfully" });
+app.post("/login", async (req, res) => {
+  let resp = {},
+    code;
+  try {
+    resp = await connection
+      .db("users")
+      .collection("users")
+      .findOne({ email: req.body.email, password: req.body.password })
+      .toArray();
+    code = 200;
+  } catch (e) {
+    code = 400;
+    console.log(e);
+  } finally {
+    res.status(code).json(resp);
+  }
 });
 
-function getUserIndex(req, res, next) {
-  let index = users.findIndex((user, index) => {
-    return user.id == req.params.id;
-  });
-  req.body.user = users[index];
-  req.body.arrIndex = index;
-  index != -1 ? next() : res.sendStatus(400);
-}
-
-// Authenticate Middleware function [with user credentials]
-function authenticate(req, res, next) {
-  users.some((user) => {
-    return user.email === req.body.email && user.password === req.body.password;
-  })
-    ? next()
-    : res.sendStatus(400);
-}
+// TODO: Authenticate Middleware function [with user credentials]
 
 app.listen(8080, () => {
   console.info("Server is running...");
