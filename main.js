66e62756473913e36470c9d0ca7fc6fc8c331f38
com194 <umang.mittal@daffodilsw.com>
2022-02-25 16:57:05 +0530

    Updated every route logic to use mongodb

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const uri = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(uri);
 const express = require("express");
@@ -7,15 +7,7 @@ const cors = require("cors");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({ origin: "*" }));
-let users = [
-  {
-    username: "ABC",
-    email: "abc@email.com",
-    password: "password",
-    id: 1,
-  },
-];
-let userId = 2;
+
 let connection;
 
 async function connectToDb() {
@@ -34,101 +26,89 @@ client.on("open", () => {
 
 //Returns all the users from the database
 app.get("/allusers", async (req, res) => {
-  let resp = {},
-    code;
+  let resp, code;
   try {
     resp = await connection.db("users").collection("users").find({}).toArray();
-    code = 200;
   } catch (e) {
     console.log(e);
-    code = 400;
   } finally {
+    resp ? (code = 200) : (code = 400);
     res.status(code).json(resp);
   }
 });
 
 // Creates a new user object into the database
 app.post("/create", async (req, res) => {
-  let resp = {},
-    code;
+  let resp, code;
   try {
     resp = await connection.db("users").collection("users").insertOne(req.body);
-    code = 200;
   } catch (e) {
-    code = 400;
     console.log(e);
   } finally {
+    resp ? (code = 200) : (code = 400);
     res.status(code).json(resp);
   }
 });
 
 // Returns the user specific to passed ID.
 app.get("/user/:id", async (req, res) => {
-  let resp = {},
-    code;
+  let resp, code;
   try {
     resp = await connection
       .db("users")
       .collection("users")
-      .find({ _id: req.params.id })
-      .toArray();
-    code = 200;
+      .findOne({ _id: new ObjectId(req.params.id) });
   } catch (e) {
     console.log(e);
-    code = 400;
   }
+  resp ? (code = 200) : (code = 400);
   res.status(code).json(resp);
 });
 
 // Edits the specific user with the passed ID.
 app.put("/user/:id", async (req, res) => {
-  let code;
+  let resp, code;
   try {
-    await connection
+    resp = await connection
       .db("users")
       .collection("users")
-      .updateOne({ _id: req.params.id }, { $set: req.body });
-    code = 200;
+      .updateOne({ _id: ObjectId(req.params.id) }, { $set: req.body });
   } catch (e) {
-    code = 400;
     console.log(e);
   } finally {
+    resp.matchedCount > 0 ? (code = 200) : (code = 400);
     res.sendStatus(code);
   }
 });
 
 // Deletes a user specific to the passed ID.
 app.delete("/user/:id", async (req, res) => {
-  let code;
+  let resp, code;
   try {
-    await connection
+    resp = await connection
       .db("users")
       .collection("users")
-      .deleteOne({ _id: req.params.id });
-    code = 202;
+      .deleteOne({ _id: ObjectId(req.params.id) });
   } catch (e) {
-    code = 400;
     console.log(e);
   } finally {
+    resp.deletedCount > 0 ? (code = 202) : (code = 400);
     res.sendStatus(code);
   }
 });
 
 //Autenticating a user based on email and password
 app.post("/login", async (req, res) => {
-  let resp = {},
-    code;
+  let resp, code;
   try {
     resp = await connection
       .db("users")
       .collection("users")
-      .findOne({ email: req.body.email, password: req.body.password })
-      .toArray();
-    code = 200;
+      .findOne({ email: req.body.email, password: req.body.password });
   } catch (e) {
-    code = 400;
     console.log(e);
   } finally {
+    resp ? (code = 200) : (code = 400);
     res.status(code).json(resp);
   }
 });
