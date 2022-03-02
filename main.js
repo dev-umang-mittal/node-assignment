12a17f6d6433bbedb6ce7f316969daa1add5401c
com194 <umang.mittal@daffodilsw.com>
2022-03-02 16:10:23 +0530

    Fixed bugs

--- main.js
+++ main.js
@@ -24,87 +24,73 @@ db.once("open", () => {
 */
 //Returns all the users from the database
 app.get("/allusers", async (req, res) => {
-  let resp, code;
   try {
-    resp = await userModel.find();
+    const response = await userModel.find();
+    res.json(response);
   } catch (e) {
-    console.log(e);
-  } finally {
-    resp ? (code = 200) : (code = 400);
-    res.status(code).json({ code, response: resp });
+    next(e);
   }
 });
 
 // Creates a new user object into the database
 app.post("/create", async (req, res, next) => {
-  let resp, code;
   const user = new userModel({
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
   });
   try {
-    resp = await user.save();
+    const response = await user.save();
+    res.json(response);
   } catch (e) {
     console.log(e);
     next(e);
-    // } finally {
-    //   resp ? (code = 200) : (code = 400);
-    //   res.status(code).json({ code, response: resp });
   }
 });
 
 // Returns the user specific to passed ID.
-app.get("/user/:id", async (req, res) => {
-  let resp, code;
+app.get("/user/:id", async (req, res, next) => {
   try {
-    resp = await userModel.findById(req.params.id);
+    const response = await userModel.findById(req.params.id);
+    res.json(response);
   } catch (e) {
-    console.log(e);
+    next(e);
   }
-  resp ? (code = 200) : (code = 400);
-  res.status(code).json({ code, response: resp });
 });
 
 // Edits the specific user with the passed ID.
-app.put("/user/:id", async (req, res) => {
-  let resp, code;
+app.put("/user/:id", async (req, res, next) => {
   try {
-    resp = await userModel.findOneAndUpdate({ _id: req.params.id }, req.body);
+    const response = await userModel.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body
+    );
+    res.json(response);
   } catch (e) {
-    console.log(e);
-  } finally {
-    resp ? (code = 200) : (code = 400);
-    res.status(code).json({ code });
+    next(e);
   }
 });
 
 // Deletes a user specific to the passed ID.
-app.delete("/user/:id", async (req, res) => {
-  let resp, code;
+app.delete("/user/:id", async (req, res, next) => {
   try {
-    resp = await userModel.findOneAndDelete({ _id: req.params.id });
+    const response = await userModel.findOneAndDelete({ _id: req.params.id });
+    res.json(response);
   } catch (e) {
-    console.log(e);
-  } finally {
-    resp ? (code = 200) : (code = 400);
-    res.status(code).json({ code });
+    next(e);
   }
 });
 
 //Autenticating a user based on email and password
-app.post("/login", async (req, res) => {
-  let resp, code;
+app.post("/login", async (req, res, next) => {
   try {
-    resp = await userModel.findOne({
+    const response = await userModel.findOne({
       email: req.body.email,
       password: req.body.password,
     });
+    res.json(response);
   } catch (e) {
-    console.log(e);
-  } finally {
-    resp ? (code = 200) : (code = 400);
-    res.status(code).json({ code, response: resp });
+    next(e);
   }
 });
 
@@ -112,34 +98,29 @@ app.post("/login", async (req, res) => {
 => Blogs ROUTES
 */
 //Get All blogs created by an author
-app.get("/blogs/:authorId", async (req, res) => {
-  let resp, code;
+app.get("/blogs/:authorId", async (req, res, next) => {
   try {
-    resp = await blogModel.find({ "authorDetails.id": req.params.authorId });
+    const response = await blogModel.find({
+      "authorDetails.id": req.params.authorId,
+    });
+    res.json(response);
   } catch (e) {
-    console.log(e);
-  } finally {
-    resp ? (code = 200) : (code = 400);
-    res.status(code).json({ code, response: resp });
+    next(e);
   }
 });
 
 //Get a blog by a specific Id
-app.get("/blog/:blogId", async (req, res) => {
-  let resp, code;
+app.get("/blog/:blogId", async (req, res, next) => {
   try {
-    resp = await blogModel.findOne({ _id: req.params.blogId });
+    const response = await blogModel.findOne({ _id: req.params.blogId });
+    res.json(response);
   } catch (e) {
-    console.log(e);
-  } finally {
-    resp ? (code = 200) : (code = 400);
-    res.status(code).json({ code, response: resp });
+    next(e);
   }
 });
 
 //Create a Blog
-app.post("/createblog", async (req, res) => {
-  let resp, code;
+app.post("/createblog", async (req, res, next) => {
   const blog = new blogModel({
     title: req.body.title,
     components: req.body.components,
@@ -148,41 +129,33 @@ app.post("/createblog", async (req, res) => {
     tags: req.body.tags,
   });
   try {
-    resp = await blog.save();
+    const response = await blog.save();
+    res.json(response);
   } catch (e) {
-    console.log(e);
-  } finally {
-    resp ? (code = 200) : (code = 400);
-    res.status(code).json({ code, response: resp });
+    next(e);
   }
 });
 
 //Delete a Blog [only if the current loggedIn user is the author]
-app.delete("/deleteblog/:blogId", async (req, res) => {
-  let resp, code;
+app.delete("/deleteblog/:blogId", async (req, res, next) => {
   try {
-    resp = await blogModel.deleteOne({ _id: req.params.blogId });
+    const response = await blogModel.deleteOne({ _id: req.params.blogId });
+    res.json(response);
   } catch (e) {
-    console.log(e);
-  } finally {
-    resp ? (code = 200) : (code = 400);
-    res.status(code).send(resp);
+    next(e);
   }
 });
 
 //Edits a Blog [Only if the current loggedIn user is the author]
-app.put("/editblog/:blogId", async (req, res) => {
-  let resp, code;
+app.put("/editblog/:blogId", async (req, res, next) => {
   try {
-    resp = await blogModel.findOneAndUpdate(
+    const response = await blogModel.findOneAndUpdate(
       { _id: req.params.blogId },
       req.body
     );
+    res.json(response);
   } catch (e) {
-    console.log(e);
-  } finally {
-    resp ? (code = 200) : (code = 400);
-    res.status(code).send(resp);
+    next(e);
   }
 });
 
@@ -191,46 +164,37 @@ app.put("/editblog/:blogId", async (req, res) => {
 */
 
 //Create a comment
-app.post("/createcomment", async (req, res) => {
-  let resp, code;
+app.post("/createcomment", async (req, res, next) => {
   const comment = new commentModel({
     commentedOnId: req.body.commentedOnId,
     commentText: req.body.commentText,
     commenter: req.body.commenter,
   });
   try {
-    resp = comment.save();
+    const response = comment.save();
+    res.json(response);
   } catch (e) {
-    console.log(e);
-  } finally {
-    resp ? (code = 200) : (code = 400);
-    res.status(code).json(resp);
+    next(e);
   }
 });
 
 //Get all comments by an Id
-app.get("/comments/:id", async (req, res) => {
-  let resp, code;
+app.get("/comments/:id", async (req, res, next) => {
   try {
-    resp = await commentModel.find({ commentedOnId: req.params.id });
+    const response = await commentModel.find({ commentedOnId: req.params.id });
+    res.json(response);
   } catch (e) {
-    console.log(e);
-  } finally {
-    resp ? (code = 200) : (code = 400);
-    res.status(code).json(resp);
+    next(e);
   }
 });
 
 //Delete a comment [only if the loggedIn user is the commenter]
-app.delete("/deletecomment/:commentId", async (req, res) => {
-  let resp, code;
+app.delete("/deletecomment/:commentId", async (req, res, next) => {
   try {
-    resp = commentModel.deleteOne({ _id: req.params.commentId });
+    const response = commentModel.deleteOne({ _id: req.params.commentId });
+    res.json(response);
   } catch (e) {
-    console.log(e);
-  } finally {
-    resp ? (code = 200) : (code = 400);
-    res.status(code).json(resp);
+    next(e);
   }
 });
 
@@ -238,15 +202,14 @@ app.delete("/deletecomment/:commentId", async (req, res) => {
 Miscellaneous Routes
 */
 //Serch route to search the blogs.
-app.get("/search/:term", async (req, res) => {
-  let resp, code;
+app.get("/search/:term", async (req, res, next) => {
   try {
-    resp = await blogModel.find({ $text: { $search: req.params.term } });
+    const response = await blogModel.find({
+      $text: { $search: req.params.term },
+    });
+    res.json(response);
   } catch (e) {
-    console.log(e);
-  } finally {
-    resp ? (code = 200) : (code = 400);
-    res.status(code).json({ code, response: resp });
+    next(e);
   }
 });
 
